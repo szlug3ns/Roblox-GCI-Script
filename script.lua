7d2e9a9de244b1f6233cbf8216351813cc4e96fd
Szlug3ns <jan7sitarski@outlook.com>
2022-05-22 15:18:09 +0200

    sheesh

---
+++ script.lua
@@ -0,0 +1,6 @@
+local mod = require(game:GetService("ReplicatedStorage").Multipliers)
+ 
+-- Dont change the first number, when you want to get more/less then change the second number
+setconstant(mod.calculateMulti, 129, 1000000000) -- Rebirth multi
+setconstant(mod.calculateMulti, 150, 1000000000) -- Crystal Multi
+setconstant(mod.calculateMulti, 56, 2000000000000000000) -- Level Multi
